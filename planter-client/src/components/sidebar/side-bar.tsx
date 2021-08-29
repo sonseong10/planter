9b65505417e509e8aff22f41696a7a25981464c5
sonseong10 <sonseong10@gmail.com>
2021-08-29 15:43:07 +0900

    🚧 Implement side bar, logo components

---
+++ planter-client/src/components/sidebar/side-bar.tsx
@@ -0,0 +1,34 @@
+import { css } from "@emotion/react"
+import media from "../../lib/styles/constants/media"
+import Logo from "../ui/logo"
+
+type SideBarProps = {}
+
+const SideBar = (_: SideBarProps) => {
+  return (
+    <div css={sideBarStyle}>
+      <Logo />
+
+      <ul css={menuStyle}></ul>
+    </div>
+  )
+}
+
+const sideBarStyle = css`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  ${media.xlarge} {
+    display: none;
+  }
+`
+
+const menuStyle = css`
+  list-style: none;
+  padding: 0;
+  margin-top: 5.625rem;
+  margin-left: -1rem;
+  flex: 1;
+`
+export default SideBar
---
+++ planter-client/src/components/ui/logo.tsx
@@ -0,0 +1,44 @@
+import { css } from "@emotion/react"
+import { Link } from "react-router-dom"
+import logoImg from "../../assets/images/logo.svg"
+import palette from "../../lib/styles/constants/palette"
+
+type LogoProps = {
+  size?: string
+}
+
+const Logo = (_: LogoProps) => {
+  return (
+    <div css={logo}>
+      <Link to="/">
+        <img src={logoImg} alt="planter" />
+      </Link>
+    </div>
+  )
+}
+
+const logo = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  overflow: hidden;
+  transition: background-color 200ms ease-in-out;
+
+  :hover {
+    background-color: ${palette.brandThemeOpacity};
+  }
+
+  img {
+    flex-grow: 1;
+    flex-shrink: 0;
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+
+export default Logo
