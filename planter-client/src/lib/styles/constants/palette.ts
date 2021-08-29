67f6d1c8b8c50604eb2d00492e1bfb91a0d816bc
sonseong10 <sonseong10@gmail.com>
2021-08-29 15:30:50 +0900

    💄 Add layout size and color palette constants

---
+++ planter-client/src/lib/styles/constants/palette.ts
@@ -0,0 +1,35 @@
+export const gray = {
+  "100": "#F5F8FA",
+  "200": "#E1E8ED",
+  "300": "#AAB8C2",
+  "400": "#657786",
+  "500": "#14171A",
+}
+
+export const text = {
+  primary: gray["500"],
+  secondary: gray["400"],
+}
+
+export const black = "#000000"
+export const white = "#ffffff"
+
+export const brandTheme = "#61f21d"
+export const brandThemeOpacity = "#61f21d45"
+
+export const border = "#eff3f4"
+
+export const background = gray["100"]
+
+const palette = {
+  gray,
+  text,
+  black,
+  white,
+  brandTheme,
+  brandThemeOpacity,
+  border,
+  background,
+}
+
+export default palette
