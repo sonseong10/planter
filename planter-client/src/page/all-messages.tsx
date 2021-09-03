425efe343b552a839e1d8fa550e82e02d0f8e94c
sonseong10 <sonseong10@gmail.com>
2021-09-03 18:49:25 +0900

    ♻️ messags lifting state up

--- planter-client/src/page/all-messages.tsx
+++ planter-client/src/page/all-messages.tsx
@@ -1,26 +1,55 @@
-import React from "react"
-import MessageForm from "../components/messages/message-form"
-import Messages from "../components/messages/messages"
-import SectionHeader from "../components/ui/section-header"
-import MessageCRUD from "../service/message"
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import MessageForm from "../components/messages/message-form";
+import Messages from "../components/messages/messages";
+import SectionHeader from "../components/ui/section-header";
+import MessageCRUD, { Message } from "../service/message";
 
 type AllMessagesProps = {
-  messageService: MessageCRUD
-}
+  messageService: MessageCRUD;
+};
 
 const AllMessages = ({ messageService }: AllMessagesProps) => {
-  const username = ""
+  const username = "";
+  const history = useHistory();
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [error, setError] = useState<string>("");
+
+  // TODO: CRUD 구현
+
+  useEffect(() => {
+    messageService
+      .getMessages(username)
+      .then((messages) => setMessages([...messages]))
+      .catch(onError);
+  }, [setMessages, messageService, username]);
+
+  const onCreated = (message: Message) => {
+    setMessages((messages) => [message, ...messages]);
+  };
+
+  const onUsernameClick = (message: Message): void =>
+    history.push(`/${message.user.name}`);
+
+  const onError = (error: any) => {
+    setError(error.toString());
+    setTimeout(() => {
+      setError("");
+    }, 3000);
+  };
+
   return (
     <>
       <SectionHeader title={"Home"} icon={"sorte"} />
-      <MessageForm />
+      <MessageForm onCreated={onCreated} />
       <Messages
-        messageService={messageService}
-        addable={true}
         username={username}
+        messages={messages}
+        onUsernameClick={onUsernameClick}
+        error={error}
       />
     </>
-  )
-}
+  );
+};
 
-export default AllMessages
+export default AllMessages;
