d13ba17d43cd0901b46d19e93ae14734891aed21
sonseong10 <sonseong10@gmail.com>
2021-09-03 10:35:08 +0900

    🐛 Fix a message api service

--- planter-client/src/service/message.ts
+++ planter-client/src/service/message.ts
@@ -1,75 +1,74 @@
 export interface MessageCRUD {
-  getMessages(username: string): Promise<JSON>
-  postMessage(text: string): object
-  deleteMessage(MessageId: string): void
-  updateMessage(MessageId: string, text: string): object
+  getMessages(username: string): Promise<Message[]>;
+  postMessage(text: string): Promise<Message[]>;
+  deleteMessage(MessageId: string): Promise<void>;
+  updateMessage(MessageId: string, text: string): Promise<Message[]>;
 }
 
 export interface Message {
-  uid: number | string
-  text: string
-  createdAt: string
-  user: UserInfo
+  uid: number | string;
+  text: string;
+  createdAt: string;
+  user: UserInfo;
 }
 
 interface UserInfo {
-  name: string
-  nickname: string
-  url?: string | undefined
+  name: string;
+  nickname: string;
+  url?: string | undefined;
 }
 
 export default class MessageService implements MessageCRUD {
   constructor(private readonly baseURL: string) {
-    this.baseURL = baseURL
+    this.baseURL = baseURL;
   }
 
-  async getMessages(username: string) {
-    const query: string = username ? `?username=${username}` : ""
+  async getMessages(username: string): Promise<Message[]> {
+    const query: string = username ? `?username=${username}` : "";
     const response = await fetch(`${this.baseURL}/${query}`, {
       method: "GET",
       headers: { "Content-Type": "application/json" },
-    })
-    const data = await response.json()
+    });
+    const data = await response.json();
     if (response.status !== 200) {
-      throw new Error()
+      throw new Error();
     }
-    return data
+    return data;
   }
 
-  async postMessage(text: string) {
+  async postMessage(text: string): Promise<Message[]> {
     const response = await fetch(`${this.baseURL}/`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text, username: "anna", name: "anna" }),
-    })
-    const data = await response.json()
+    });
+    const data = await response.json();
     if (response.status !== 200) {
-      throw new Error(data.error)
+      throw new Error(data.error);
     }
-    return data
+    return data;
   }
 
-  async deleteMessage(messageId: string) {
+  async deleteMessage(messageId: string): Promise<void> {
     const response = await fetch(`${this.baseURL}/${messageId}`, {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
-    })
-    const data = await response.json()
+    });
     if (response.status !== 204) {
-      throw new Error(data.error)
+      throw new Error();
     }
   }
 
-  async updateMessage(messageId: string, text: string) {
+  async updateMessage(messageId: string, text: string): Promise<Message[]> {
     const response = await fetch(`${this.baseURL}/${messageId}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text }),
-    })
-    const data = await response.json()
+    });
+    const data = await response.json();
     if (response.status !== 200) {
-      throw new Error(data.error)
+      throw new Error(data.error);
     }
-    return data
+    return data;
   }
 }
